29526209df0c3c94750e848b46158fc831fd1790
Eugene Sadovoi <evgeni@eniks.com>
2017-09-13 17:11:27 -0400

    Refactoring yet again. - Added latest tools, targets, and frameworks. - Added coverage display - Added auto deployment

---
+++ src/IServiceProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.Practices.ServiceLocation
+{
+    //
+    // Summary:
+    //     Defines a mechanism for retrieving a service object; that is, an object that
+    //     provides custom support to other objects.
+    public interface IServiceProvider
+    {
+        //
+        // Summary:
+        //     Gets the service object of the specified type.
+        //
+        // Parameters:
+        //   serviceType:
+        //     An object that specifies the type of service object to get.
+        //
+        // Returns:
+        //     A service object of type serviceType.-or- null if there is no service object
+        //     of type serviceType.
+        object GetService(System.Type serviceType);
+    }
+}
---
+++ test/ServiceLocatorAdapterFixture.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Practices.ServiceLocation;
+using ServiceLocation.Tests.Components;
+using ServiceLocation.Tests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceLocation.Tests
+{
+ 
+    [TestClass]
+    public class ServiceLocatorAdapterFixture
+    {
+        protected IServiceLocator locator;
+
+        [TestInitialize]
+        public void InitAdapterFixture()
+        {
+            locator = new MockServiceLocator(new object[] { new SimpleLogger(),
+                                             new AdvancedLogger(),
+                                             new NullReferenceException() });
+        }
+
+        [TestMethod]
+        public void GetInstance()
+        {
+            ILogger instance = locator.GetInstance<ILogger>();
+            Assert.IsNotNull(instance);
+        }
+
+
+        [TestMethod]
+        public void GetNamedInstance()
+        {
+            ILogger instance = locator.GetInstance<ILogger>(typeof(AdvancedLogger).FullName);
+            Assert.IsInstanceOfType(instance, typeof(AdvancedLogger));
+        }
+    }
+}
